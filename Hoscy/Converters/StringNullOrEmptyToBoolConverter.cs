99a870be1b1d7a5ea020603c7a87c9dd27c2c185
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-09-25 10:05:26 +0200

    StringNullOrEmptyToBoolConverter

---
+++ Hoscy/Converters/StringNullOrEmptyToBoolConverter.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Hoscy.Converters;
+
+public class StringNullOrEmptyToBoolConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var s = value as string;
+            return string.IsNullOrEmpty(s);
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
