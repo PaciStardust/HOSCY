d3e56825632349f305670797d8cd06e6d1bcf5d1
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-01 23:05:14 +0200

    Interface for BackToFrontNotify

---
+++ Hoscy/Services/Interfacing/IBackToFrontNotifyService.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Hoscy.Services.Interfacing;
+
+public interface IBackToFrontNotifyService
+{
+    public event EventHandler<BackToFrontNotifyEventArgs> OnInfo;
+    public event EventHandler<BackToFrontNotifyEventArgs> OnWarning;
+    public event EventHandler<BackToFrontNotifyEventArgs> OnError;
+    public event EventHandler<BackToFrontNotifyEventArgs> OnFatal;
+
+    public void SendInfo(string title, string content, Exception? exception = null);
+    public void SendWarning(string title, string content, Exception? exception = null);
+    public void SendError(string title, string content, Exception? exception = null);
+    public void SendFatal(string title, string content, Exception? exception = null);
+}
+
