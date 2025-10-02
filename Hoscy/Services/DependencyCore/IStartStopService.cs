69fb4af4173f8b7464a1cfe1588c577cbf140c06
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-02 08:55:43 +0200

    Added status indication to StartStopService

--- Hoscy/Services/DependencyCore/IStartStopService.cs
+++ Hoscy/Services/DependencyCore/IStartStopService.cs
@@ -1,8 +1,24 @@
+using System;
+
 namespace Hoscy.Services.DependencyCore;
 
 public interface IStartStopService
 {
-    void Start();
-    void Stop();
-    bool IsFaulted();
+    public void Start();
+    public void Stop();
+    public Exception? GetFaultIfExists();
+    public bool IsRunning();
+
+    public StatStopServiceStatus GetStatus()
+    {
+        if (!IsRunning()) return StatStopServiceStatus.Stopped;
+        if (GetFaultIfExists() is not null) return StatStopServiceStatus.Faulted;
+        return StatStopServiceStatus.Running;
+    }
+}
+
+public enum StatStopServiceStatus {
+    Running,
+    Stopped,
+    Faulted
 }
