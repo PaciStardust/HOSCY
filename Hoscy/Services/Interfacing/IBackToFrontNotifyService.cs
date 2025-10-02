46b7771d4dd640c9bd692d5a3e88a719ff1faaea
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-02 10:07:36 +0200

    Default value for Notify Content

--- Hoscy/Services/Interfacing/IBackToFrontNotifyService.cs
+++ Hoscy/Services/Interfacing/IBackToFrontNotifyService.cs
@@ -9,9 +9,9 @@ public interface IBackToFrontNotifyService
     public event EventHandler<BackToFrontNotifyEventArgs> OnError;
     public event EventHandler<BackToFrontNotifyEventArgs> OnFatal;
 
-    public void SendInfo(string title, string content, Exception? exception = null);
-    public void SendWarning(string title, string content, Exception? exception = null);
-    public void SendError(string title, string content, Exception? exception = null);
-    public void SendFatal(string title, string content, Exception? exception = null);
+    public void SendInfo(string title, string content = "", Exception? exception = null);
+    public void SendWarning(string title, string content = "", Exception? exception = null);
+    public void SendError(string title, string content = "", Exception? exception = null);
+    public void SendFatal(string title, string content = "", Exception? exception = null);
 }
 
