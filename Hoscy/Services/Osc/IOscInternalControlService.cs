8e131e80d0757f7652eca8f5bf1f3208775d9dc8
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-05 12:22:24 +0200

    More docu for osc

--- Hoscy/Services/Osc/IOscInternalControlService.cs
+++ Hoscy/Services/Osc/IOscInternalControlService.cs
@@ -2,6 +2,9 @@ using LucHeart.CoreOSC;
 
 namespace Hoscy.Services.Osc;
 
+/// <summary>
+/// Service for routing OSC to internal tooling
+/// </summary>
 public interface IOscInternalControlService
 {
     public bool HandleInternalControl(OscMessage message);
