c8ca5c12172ad35b5f9a7eafade5ad619c63d7a5
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-07 12:47:39 +0200

    IOscCommandService

---
+++ Hoscy/Services/Osc/IOscCommandService.cs
@@ -0,0 +1,19 @@
+namespace Hoscy.Services.Osc;
+
+/// <summary>
+/// Handling of OSC commands
+/// </summary>
+public interface IOscCommandService
+{
+    /// <summary>
+    /// Checks if a string of text is an OSC command
+    /// </summary>
+    /// <returns>Is Command?</returns>
+    public bool DetectCommand(string commandString);
+
+    /// <summary>
+    /// Checks if a string of text is an OSC command and executes it
+    /// </summary>
+    /// <returns>Success</returns>
+    public bool DetectAndHandleCommand(string commandString);
+}
