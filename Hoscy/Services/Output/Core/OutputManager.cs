af0c608a6be2756064b1e600d5a317e2a75e76c2
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-08 12:53:57 +0200

    OutputManager > Adding necessary variables

--- Hoscy/Services/Output/Core/OutputManager.cs
+++ Hoscy/Services/Output/Core/OutputManager.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections.Generic;
 using Hoscy.Services.DependencyCore;
+using Serilog;
 
 namespace Hoscy.Services.Output.Core;
 
 [LoadIntoDiContainer(typeof(IOutputManager), Lifetime.Singleton)]
-public class OutputManager : StartStopServiceBase, IOutputManager
+public class OutputManager(ILogger logger) : StartStopServiceBase, IOutputManager
 {
+    #region Injected
+    private readonly ILogger _logger = logger.ForContext<OutputManager>();
+    #endregion
+
+    #region Service Vars
+    private readonly List<OutputProcessorInfo> _availableProcessors = [];
+    #endregion
+
     #region Events
     public event EventHandler<string> OnMessage = delegate {};
     public event EventHandler<OutputNotificationEventArgs> OnNotification = delegate {};
