1d5041963feb2efd480ccf531763b7763329e738
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-08 12:20:18 +0200

    Tweaks IOutputProcessor

--- Hoscy/Services/Output/Core/IOutputProcessor.cs
+++ Hoscy/Services/Output/Core/IOutputProcessor.cs
@@ -4,14 +4,28 @@ namespace Hoscy.Services.Output.Core;
 
 public interface IOutputProcessor
 {
+    #region Information
+    /// <summary>
+    /// Get information associated with this processor
+    /// </summary>
+    public OutputProcessorInfo GetInfo();
+    /// <summary>
+    /// Event that triggers when the process encounteres an Exception outside of starting and stopping
+    /// </summary>
+    public event EventHandler<Exception> OnRuntimeError;
+    #endregion
+
+    #region Start / Stop
     public void Activate();
     public void Shutdown();
     public void Restart();
     public void IsActive();
-    public OutputProcessorInfo GetInfo();
-    public event Action<Exception> OnRuntimeError;
+    #endregion
+
+    #region Functionality
     public bool SendMessage(string contents);
     public bool SendNotification(string contents, OutputNotificationPriority priority);
     public bool Clear();
     public bool SetProcessingIndicator(bool isProcessing);
+    #endregion
 }
