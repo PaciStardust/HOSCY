e03a49a763f9b4a7767bb2be165195f8fb80413d
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-09 11:41:59 +0200

    Removed IDispose from IStartStopService

--- Hoscy/Services/DependencyCore/IStartStopService.cs
+++ Hoscy/Services/DependencyCore/IStartStopService.cs
@@ -5,18 +5,12 @@ namespace Hoscy.Services.DependencyCore;
 /// <summary>
 /// Represents a service that can be started and stopped
 /// </summary>
-public interface IStartStopService : IDisposable
+public interface IStartStopService
 {
     public void Start();
     public void Stop();
     public void Restart();
-    public bool IsRunning(); //todo: needed?
+    public bool IsRunning();
     public StartStopStatus GetStatus();
     public Exception? GetFaultIfExists();
-
-    void IDisposable.Dispose()
-    {
-        Stop();
-        GC.SuppressFinalize(this);
-    }
 }
