e7de0113c3bb7268545c426ea5f8d6384a7584a4
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-16 13:57:17 +0200

    Turns out writing a proper text splitter with low memory overhead is a bit more tricky than initially thought

--- Hoscy/Services/Output/Textbox/VrcTextboxOutputProcessor.cs
+++ Hoscy/Services/Output/Textbox/VrcTextboxOutputProcessor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Hoscy.Configuration.Modern;
 using Hoscy.Services.DependencyCore;
 using Hoscy.Services.Osc.SendReceive;
@@ -45,6 +46,108 @@ public class VrcTextboxOutputProcessor(ILogger logger, ConfigModel config, IOscS
     };
     #endregion
 
+    #region Processing Indicator
+    /// <summary>
+    /// Enables typing indicator for textbox
+    /// Note: This only stays on for 5 seconds ingame
+    /// </summary>
+    public void SetProcessingIndicator(bool isProcessing)
+    {
+        if (!isProcessing && !_lastSetProcessingState) return;
+        if (!CanSetProcessingIndicator()) return;
+
+        _lastSentTypingIndicator = isProcessing ? DateTime.Now : DateTime.MinValue;
+        _lastSetProcessingState = isProcessing;
+
+        _sender.SendToDefaultSyncFireAndForget("/chatbox/typing", isProcessing ? 1 : 0); //todo: should this be fire and forget?
+    }
+
+    private bool CanSetProcessingIndicator()
+    {
+        return _lastSentTypingIndicator.AddSeconds(4) > DateTime.Now
+            && _config.Textbox_Text_TypingIndicatorWhenSpeaking
+            && (_config.Input_UseTextbox || _config.Textbox_Text_TypingIndicatorWhenDisabled); //todo: UseTextbox check needs a redo
+    }
+    #endregion
+
+    #region Input Processing
+    private const string SPLIT_LONG_WORD_CUTOFF = "-";
+    private const string SPLIT_HAS_PREVIOUS_INDICATOR = "... ";
+    private const string SPLIT_HAS_AFTER_INDICATOR = " ...";
+    /// <summary>
+    /// Splitting message into displayable segments
+    /// </summary>
+    /// <returns>Split message</returns>
+    private List<string> SplitMessageIntoSegments(string message)
+    {
+        var segments = new List<(int, int)>(); //Should be pairs of values => 1st is start index, 2nd is length
+		var currentSegmentStart = -1;
+		var currentWordStart = -1;
+		var currentSegmentPotentialEnd = -1;
+        var maxLength = _config.Textbox_Text_MaxDisplayedCharacters;
+        for (var charIndex = 0; charIndex < message.Length; charIndex++)
+        {
+            var isWordSeparator = message[charIndex] == ' ' || message[charIndex] == '\r' || message[charIndex] == '\n' || message[charIndex] == '\t';
+            if (currentWordStart == -1) // We were not in a word
+            {
+                if (!isWordSeparator) //We are now in a word
+                {
+					if (currentSegmentStart == -1) {
+                    	currentSegmentStart = charIndex;
+					}
+                    currentWordStart = charIndex;
+                }
+                continue;
+            }
+
+            if (!isWordSeparator) continue; //Do not do any logic if we are still in a word
+			
+            var expectedLength = charIndex - currentSegmentStart;
+            if (expectedLength <= maxLength) //Completed word is within limits
+            {
+                currentWordStart = -1;
+                currentSegmentPotentialEnd = charIndex;
+                continue;
+            }
+
+            if (currentSegmentPotentialEnd != -1) //Not the first word in segment => At least 1 word already fit in
+            {
+				segments.Add((currentSegmentStart, currentSegmentPotentialEnd - currentSegmentStart));
+				currentSegmentPotentialEnd = -1;
+				currentSegmentStart = currentWordStart;
+            }
+            else //This word is the first word and too long for bounds
+            {
+				segments.Add((currentSegmentStart, charIndex - currentSegmentStart));
+                currentSegmentStart = -1;
+            }
+			currentWordStart = -1;
+        }
+        if (currentSegmentStart != -1) //Handles loop ending on a valid character
+        {
+			segments.Add((currentSegmentStart, message.Length - currentSegmentStart));
+        }
+
+        var messageSegments = new List<string>();
+        if (segments.Count == 0) return messageSegments;
+
+        for (var i = 0; i < segments.Count; i++)
+        {
+            var length = segments[i].Item2;
+            var aboveLimits = false;
+            if (length > maxLength)
+            {
+                length = maxLength - 1;
+                aboveLimits = true;
+            }
+			
+            var text = $"{(i > 0 ? SPLIT_HAS_PREVIOUS_INDICATOR : string.Empty)}{message.Substring(segments[i].Item1, length)}{(aboveLimits ? SPLIT_LONG_WORD_CUTOFF : string.Empty)}{(i + 2 < segments.Count ? SPLIT_HAS_AFTER_INDICATOR : string.Empty)}";
+            messageSegments.Add(text);
+        }
+		return messageSegments;
+    }
+    #endregion
+
     public void Activate()
     {
         throw new NotImplementedException();
@@ -75,7 +178,7 @@ public class VrcTextboxOutputProcessor(ILogger logger, ConfigModel config, IOscS
         throw new NotImplementedException();
     }
 
-    public void ProcessMessage(string contents)
+    public void ProcessMessage(string contents)  
     {
         throw new NotImplementedException();
     }
@@ -83,28 +186,6 @@ public class VrcTextboxOutputProcessor(ILogger logger, ConfigModel config, IOscS
     public void ProcessNotification(string contents, OutputNotificationPriority priority)
     {
         throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Enables typing indicator for textbox
-    /// Note: This only stays on for 5 seconds ingame
-    /// </summary>
-    public void SetProcessingIndicator(bool isProcessing)
-    {
-        if (!isProcessing && !_lastSetProcessingState) return;
-        if (!CanSetProcessingIndicator()) return;
-
-        _lastSentTypingIndicator = isProcessing ? DateTime.Now : DateTime.MinValue;
-        _lastSetProcessingState = isProcessing;
-
-        _sender.SendToDefaultSyncFireAndForget("/chatbox/typing", isProcessing ? 1 : 0); //todo: should this be fire and forget?
-    }
-
-    private bool CanSetProcessingIndicator()
-    {
-        return _lastSentTypingIndicator.AddSeconds(4) > DateTime.Now
-            && _config.Textbox_Text_TypingIndicatorWhenSpeaking
-            && (_config.Input_UseTextbox || _config.Textbox_Text_TypingIndicatorWhenDisabled); //todo: UseTextbox check needs a redo
     }
 
     public void Shutdown()
