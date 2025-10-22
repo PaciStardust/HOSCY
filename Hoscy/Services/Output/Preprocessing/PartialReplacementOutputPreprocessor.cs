f3c748f95dee02c804ac927aa69d5cc96956858b
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-22 17:40:51 +0200

    PartialReplacementOutputPreprocessor > Converted to use Base

--- Hoscy/Services/Output/Preprocessing/PartialReplacementOutputPreprocessor.cs
+++ Hoscy/Services/Output/Preprocessing/PartialReplacementOutputPreprocessor.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using Hoscy.Configuration.Modern;
 using Hoscy.Services.DependencyCore;
@@ -9,79 +8,31 @@ using Serilog;
 
 namespace Hoscy.Services.Output.Preprocessing;
 
-[LoadIntoDiContainer(typeof(PartialReplacementOutputPreprocessor), Lifetime.Singleton)]
 /// <summary>
 /// Handles partial replacements of text
 /// </summary>
-public class PartialReplacementOutputPreprocessor : IOutputPreprocessor //todo: base class?
+[LoadIntoDiContainer(typeof(PartialReplacementOutputPreprocessor), Lifetime.Singleton)]
+public class PartialReplacementOutputPreprocessor(ConfigModel config, ILogger logger) : ReplacementOutputPreprocessorBase<PartialReplacementHandler>(config, logger.ForContext<PartialReplacementOutputPreprocessor>())
 {
-    private readonly ConfigModel _config;
-    private readonly ILogger _logger;
-    private readonly List<PartialReplacementHandler> _handlers = [];
+    #region Simple Overrides
+    public override PartialReplacementHandler ConvertToHandler(ReplacementDataModel model)
+        => new(model);
 
-    public PartialReplacementOutputPreprocessor(ConfigModel config, ILogger logger)
-    {
-        _config = config;
-        _logger = logger.ForContext<PartialReplacementOutputPreprocessor>();
-        ReloadPartialReplacements();
-        config.PropertyChanged += OnPropertyChanged;
-    }
+    public override string GetReloadPropertyName()
+        => nameof(_config.Speech_Replacement_Partial);
 
-    #region Info
-    public OutputPreprocessorHandlingStage GetHandlingStage()
+    public override ObservableCollection<ReplacementDataModel> GetReplacementModels()
+        => _config.Speech_Replacement_Partial;
+
+    public override OutputPreprocessorHandlingStage GetHandlingStage()
         => OutputPreprocessorHandlingStage.Alter;
 
-    public bool ShouldContinueIfHandled()
+    public override bool ShouldContinueIfHandled()
         => true;
     #endregion
 
-
-    #region Reload
-    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e) //todo: does this work?
-    {
-        if (e.PropertyName == nameof(_config.Speech_Replacement_Partial))
-        {
-            ReloadPartialReplacements();
-        }
-    }
-
-    private void ReloadPartialReplacements()
-    {
-        _logger.Information("Reloading {replacementModelCount} partial replacements", _config.Speech_Replacement_Partial.Count);
-        _handlers.Clear();
-
-        var converted = new List<PartialReplacementHandler>();
-        var countDisabled = 0;
-        var countBroken = 0;
-
-        foreach (var replacement in _config.Speech_Replacement_Partial)
-        {
-            if (!replacement.Enabled)
-            {
-                countDisabled++;
-                continue;
-            }
-
-            try
-            {
-                var handler = new PartialReplacementHandler(replacement);
-                converted.Add(handler);
-            }
-            catch (Exception ex)
-            {
-                _logger.Warning(ex, "Could not instantiate partial replacement \"{replacementText}\", RegEx might be broken => Skipping and disabling", replacement.Text);
-                replacement.Enabled = false;
-                countBroken++;
-            }
-        }
-
-        _handlers.AddRange(converted);
-        _logger.Information("Reloaded {replacementModelCountLoaded}/{replacementModelCount} patrial replacements, {disabledCount} disabled, {brokenCount} broken", _handlers.Count, _config.Speech_Replacement_Partial.Count, countDisabled, countBroken);
-    }
-    #endregion
-
     #region Processing
-    public bool TryProcess(string input, [NotNullWhen(true)] out string? output)
+    public override bool TryProcess(string input, [NotNullWhen(true)] out string? output)
     {
         output = null;
         foreach (var handler in _handlers) //todo: clean this
