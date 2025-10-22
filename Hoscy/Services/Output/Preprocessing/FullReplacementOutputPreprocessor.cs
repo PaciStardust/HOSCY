a9920a217977350372cca6423c83ce388bdbf702
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-22 12:24:40 +0200

    FullReplacementOutputPreprocessor > Impl Functionality

--- Hoscy/Services/Output/Preprocessing/FullReplacementOutputPreprocessor.cs
+++ Hoscy/Services/Output/Preprocessing/FullReplacementOutputPreprocessor.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-using Avalonia.Logging;
 using Hoscy.Configuration.Modern;
 using Hoscy.Services.Output.Core;
 using Serilog;
 
 namespace Hoscy.Services.Output.Preprocessing;
 
-
+/// <summary>
+/// Handles replacing text fully
+/// </summary>
 public class FullReplacementOutputPreprocessor : IOutputPreprocessor
 {
     private readonly ConfigModel _config;
@@ -21,25 +22,19 @@ public class FullReplacementOutputPreprocessor : IOutputPreprocessor
         _config = config;
         ReloadFullReplacements();
         config.PropertyChanged += OnPropertyChanged;
-        _logger = logger;
+        _logger = logger.ForContext<FullReplacementOutputPreprocessor>();
     }
 
+    #region Info
     public OutputPreprocessorHandlingStage GetHandlingStage()
-    {
-        throw new NotImplementedException();
-    }
+        => OutputPreprocessorHandlingStage.Replace;
 
     public bool ShouldContinueIfHandled()
-    {
-        throw new NotImplementedException();
-    }
+        => true;
+    #endregion
 
-    public bool TryProcess(string input, [NotNullWhen(true)] out string? output)
-    {
-        throw new NotImplementedException();
-    }
-
-    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    #region Reload
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e) //todo: does this work?
     {
         if (e.PropertyName == nameof(_config.Speech_Replacement_Full))
         {
@@ -80,4 +75,21 @@ public class FullReplacementOutputPreprocessor : IOutputPreprocessor
         _handlers.AddRange(converted);
         _logger.Information("Reloaded {replacementModelCountLoaded}/{replacementModelCount} full replacements, {disabledCount} disabled, {brokenCount} broken", _handlers.Count, _config.Speech_Replacement_Full.Count, countDisabled, countBroken);
     }
+    #endregion
+
+    #region Processing
+    public bool TryProcess(string input, [NotNullWhen(true)] out string? output)
+    {
+        foreach (var handler in _handlers)
+        {
+            if (!handler.Compare(input)) continue;
+
+            output = handler.GetReplacement();
+            return true;
+        }
+
+        output = null;
+        return false;
+    }
+    #endregion
 }
