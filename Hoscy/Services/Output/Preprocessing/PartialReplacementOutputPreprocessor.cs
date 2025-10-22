e7777e8804f399cd2bf8ee629cc573370bfd9ae5
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-22 12:40:13 +0200

    PartialReplacementOutputPreprocessor > Inject & Reload

--- Hoscy/Services/Output/Preprocessing/PartialReplacementOutputPreprocessor.cs
+++ Hoscy/Services/Output/Preprocessing/PartialReplacementOutputPreprocessor.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using Hoscy.Configuration.Modern;
+using Hoscy.Services.DependencyCore;
 using Hoscy.Services.Output.Core;
 using Serilog;
 
 namespace Hoscy.Services.Output.Preprocessing;
 
-public class PartialReplacementOutputPreprocessor : IOutputPreprocessor
+[LoadIntoDiContainer(typeof(FullReplacementOutputPreprocessor), Lifetime.Singleton)]
+/// <summary>
+/// Handles partial replacements of text
+/// </summary>
+public class PartialReplacementOutputPreprocessor : IOutputPreprocessor //todo: base class?
 {
     private readonly ConfigModel _config;
     private readonly ILogger _logger;
@@ -30,15 +35,50 @@ public class PartialReplacementOutputPreprocessor : IOutputPreprocessor
         => true;
     #endregion
 
-    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+
+    #region Reload
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e) //todo: does this work?
     {
-        throw new NotImplementedException();
+        if (e.PropertyName == nameof(_config.Speech_Replacement_Partial))
+        {
+            ReloadPartialReplacements();
+        }
     }
 
     private void ReloadPartialReplacements()
     {
-        throw new NotImplementedException();
+        _logger.Information("Reloading {replacementModelCount} partial replacements", _config.Speech_Replacement_Partial.Count);
+        _handlers.Clear();
+
+        var converted = new List<PartialReplacementHandler>();
+        var countDisabled = 0;
+        var countBroken = 0;
+
+        foreach (var replacement in _config.Speech_Replacement_Partial)
+        {
+            if (!replacement.Enabled)
+            {
+                countDisabled++;
+                continue;
+            }
+
+            try
+            {
+                var handler = new PartialReplacementHandler(replacement);
+                converted.Add(handler);
+            }
+            catch (Exception ex)
+            {
+                _logger.Warning(ex, "Could not instantiate partial replacement \"{replacementText}\", RegEx might be broken => Skipping and disabling", replacement.Text);
+                replacement.Enabled = false;
+                countBroken++;
+            }
+        }
+
+        _handlers.AddRange(converted);
+        _logger.Information("Reloaded {replacementModelCountLoaded}/{replacementModelCount} patrial replacements, {disabledCount} disabled, {brokenCount} broken", _handlers.Count, _config.Speech_Replacement_Partial.Count, countDisabled, countBroken);
     }
+    #endregion
 
     public bool TryProcess(string input, [NotNullWhen(true)] out string? output)
     {
