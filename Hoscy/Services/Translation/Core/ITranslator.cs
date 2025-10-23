45559c25ccef6b70e3bb170af6bf1a7951c76fd1
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-23 21:13:58 +0200

    ITranslator

---
+++ Hoscy/Services/Translation/Core/ITranslator.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+using Hoscy.Services.DependencyCore;
+
+namespace Hoscy.Services.Translation.Core;
+
+public interface ITranslator : IStartStopService
+{
+    public string GetName();
+    public bool TryTranslate(string input, [NotNullWhen(true)] string? output);
+}
