cd9816f85848eb2759917c48ac70c9b91d28f6d1
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-23 21:14:11 +0200

    ITranslatorManagerService

---
+++ Hoscy/Services/Translation/Core/ITranslatorManagerService.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Hoscy.Services.DependencyCore;
+
+namespace Hoscy.Services.Translation.Core;
+
+public interface ITranslatorManagerService : IStartStopService
+{
+    #region Info
+    public IReadOnlyList<string> GetAvailableNames();
+    public string? GetCurrentName();
+    #endregion
+
+    #region Start / Stop
+    public void StartTranslator(string name);
+    public void StopCurrentTranslator();
+    public void RestartCurrentTranslator();
+    public StartStopStatus GetCurrentTranslatorStatus();
+    #endregion
+
+    #region Functionality
+    public bool TryTranslate(string input, [NotNullWhen(true)] string? output);
+    #endregion
+}
