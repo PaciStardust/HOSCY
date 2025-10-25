e61641b12e316421c45c9048ae43112bfc8e7dc7
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-25 11:35:27 +0200

    WebClient > SendAsync

--- Hoscy/Services/Network/WebClient.cs
+++ Hoscy/Services/Network/WebClient.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Hoscy.Services.DependencyCore;
+using Hoscy.Services.Interfacing;
 using Serilog;
 
 namespace Hoscy.Services.Network;
 
 [LoadIntoDiContainer(typeof(IWebClient), Lifetime.Singleton)]
-public class WebClient(ILogger logger) : StartStopServiceBase, IWebClient
+public class WebClient(ILogger logger, IBackToFrontNotifyService notify) : StartStopServiceBase, IWebClient
 {
     private readonly ILogger _logger = logger.ForContext<WebClient>();
+    private readonly IBackToFrontNotifyService _notify = notify;
     private HttpClient? _client = null;
 
     #region Start / Stop
@@ -46,13 +49,55 @@ public class WebClient(ILogger logger) : StartStopServiceBase, IWebClient
     }
     #endregion
 
+    #region Functionality
     public Task<bool> DownloadAsync(string sourceUrl, string fileLocation, int timeoutMs = 5000)
     {
         throw new System.NotImplementedException();
     }
 
-    public Task<string?> SendAsync(HttpRequestMessage requestMessage, int timeoutMs = 5000)
+    public async Task<string?> SendAsync(HttpRequestMessage requestMessage, int timeoutMs = 5000)
     {
-        throw new System.NotImplementedException();
+        var identifier = IWebClient.GetRequestIdentifier();
+        _logger.Debug("{identifier} => Sending {requestMethod} request to {requestUri}", identifier, requestMessage.Method, requestMessage.RequestUri);
+
+        if (_client is null)
+        {
+            _logger.Error("{identifier}: Failed sending, HttpClient is not initialized", identifier);
+            return null;
+        }
+
+        var startTime = DateTime.Now;
+        try
+        {
+            var cts = new CancellationTokenSource(timeoutMs);
+            var response = await _client.SendAsync(requestMessage, cts.Token);
+            var jsonIn = await response.Content.ReadAsStringAsync(cts.Token);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.Error("{identifier}: Request has received status code \"{responseStatusCode}\" ({intResponseStatusCode}){responseJson}",
+                    identifier, response.StatusCode, (int)response.StatusCode, string.IsNullOrWhiteSpace(jsonIn) ? "" : $" ({jsonIn})");
+                _notify.SendError("Failed Web Request", $"Request to {requestMessage.RequestUri} failed, see logs for more details"); //todo: more details?
+                return null;
+            }
+
+            _logger.Debug("{identifier}: Received data from request in {timePassed}ms => {jsonIn}", identifier, (DateTime.Now - startTime).TotalMilliseconds, jsonIn);
+            return jsonIn;
+        }
+        catch (Exception ex)
+        {
+            if ((ex is TaskCanceledException tce && tce.CancellationToken.IsCancellationRequested) || ex is OperationCanceledException)
+            {
+                _logger.Warning("{identifier}: Request to {requestUri} timed out", identifier, requestMessage.RequestUri);
+                _notify.SendWarning($"Request to {requestMessage.RequestUri} timed out after {timeoutMs}ms");
+            }
+            else
+            {
+                _logger.Error(ex, "{identifier}: Request to {requestUri} failed", identifier, requestMessage.RequestUri);
+                _notify.SendError("Failed Web Request", $"Request to {requestMessage.RequestUri} failed, see logs for more details", ex); //todo: more details?
+            }
+        }
+        return null;
     }
+    #endregion
 }
