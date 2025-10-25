aa82c4b0a6b2c5c429c67d90b7930800be5d747b
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-25 11:17:21 +0200

    WebClient > Start/Stop

--- Hoscy/Services/Network/WebClient.cs
+++ Hoscy/Services/Network/WebClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Hoscy.Services.DependencyCore;
@@ -9,34 +10,48 @@ namespace Hoscy.Services.Network;
 public class WebClient(ILogger logger) : StartStopServiceBase, IWebClient
 {
     private readonly ILogger _logger = logger.ForContext<WebClient>();
-    private readonly HttpClient? _client = null;
+    private HttpClient? _client = null;
+
+    #region Start / Stop
+    protected override void StartInternal()
+    {
+        _logger.Information("Setting up HttpClient");
+        var client = new HttpClient(new SocketsHttpHandler()
+        {
+            PooledConnectionLifetime = TimeSpan.FromMinutes(1),
+            UseProxy = false,
+        });
+
+        //Below is required for Github Access
+        client.DefaultRequestHeaders.UserAgent.Add(new("User-Agent", "request"));
+        _logger.Information("HttpClient set up");
+    }
+
+    public override void Stop()
+    {
+        _logger.Information("Disposing of HttpClient");
+        _client?.Dispose();
+        _client = null;
+        _logger.Information("Disposed of HttpClient");
+    }
+
+    public override bool IsRunning()
+    {
+        return _client is not null;
+    }
+    
+    public override void Restart()
+    {
+        RestartSimple(GetType().Name, _logger);
+    }
+    #endregion
 
     public Task<bool> DownloadAsync(string sourceUrl, string fileLocation, int timeoutMs = 5000)
     {
         throw new System.NotImplementedException();
     }
 
-    public override bool IsRunning()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void Restart()
-    {
-        throw new System.NotImplementedException();
-    }
-
     public Task<string?> SendAsync(HttpRequestMessage requestMessage, int timeoutMs = 5000)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void Stop()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void StartInternal()
     {
         throw new System.NotImplementedException();
     }
