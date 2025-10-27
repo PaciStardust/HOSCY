0f554d68cdd87ad3463405de8536f19424abea0c
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-27 07:32:13 +0100

    Added some todos for refurb

--- Hoscy/Services/Translation/Core/ITranslator.cs
+++ Hoscy/Services/Translation/Core/ITranslator.cs
@@ -4,7 +4,7 @@ using Hoscy.Services.DependencyCore;
 
 namespace Hoscy.Services.Translation.Core;
 
-public interface ITranslator : IStartStopService
+public interface ITranslator : IStartStopService //todo: add an autostart field so these do not get started up on load, redo bases
 {
     public event EventHandler<Exception> OnRuntimeError;
     public event EventHandler OnShutdownCompleted;
