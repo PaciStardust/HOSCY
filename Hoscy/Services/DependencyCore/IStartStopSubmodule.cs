bfdefe12c7c06a311bbe2676063f2b8d8794a158
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-10-27 13:02:41 +0100

    IStartStopSubmodule

--- Hoscy/Services/DependencyCore/IStartStopSubmodule.cs
+++ Hoscy/Services/DependencyCore/IStartStopSubmodule.cs
@@ -4,7 +4,13 @@ namespace Hoscy.Services.DependencyCore;
 
 public interface IStartStopSubmodule<Tidentifier> : IStartStopService
 {
+    /// <summary>
+    /// Event that triggers when the process encounteres an Exception outside of starting and stopping
+    /// </summary>
     public event EventHandler<Exception> OnRuntimeError;
+    /// <summary>
+    /// Event gets called when the processor is stopped
+    /// </summary>
     public event EventHandler OnShutdownCompleted;
     public Tidentifier GetIdentifier();
 }
