706b12786c752e630b6f67b740081c34f587cb06
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-12-19 22:22:49 +0100

    fix: :adhesive_bandage: Added audio device enumeration to IAudioService

--- Hoscy/Services/Audio/IAudioService.cs
+++ Hoscy/Services/Audio/IAudioService.cs
@@ -4,5 +4,6 @@ namespace Hoscy.Services.Audio;
 
 public interface IAudioService : IAutoStartStopService
 {
-    
+    public SoundFlow.Structs.DeviceInfo[]? GetCaptureDevices();
+    public SoundFlow.Structs.DeviceInfo[]? GetPlaybackDevices();
 }
