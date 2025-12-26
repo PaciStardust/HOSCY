0c122b705b882df768ab0e4952b35ee65187a61e
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-12-26 16:40:22 +0100

    feat(CLI): :sparkles: It lives!

---
+++ HoscyCli/CliCoreWrapper.cs
@@ -0,0 +1,112 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using HoscyCli.Commands.Core;
+using HoscyCli.Commands.Modules;
+using HoscyCore;
+using HoscyCore.Utility;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace HoscyCli;
+
+public class CliCoreWrapper
+{
+    private HoscyCoreApp? _coreApp = null;
+    private Process? _debugProcess = null;
+    private ILogger? _logger = null;
+
+    public void Start()
+    {
+        if (_coreApp is not null) return;
+        _logger = LogUtils.CreateTemporaryLogger<CliCoreWrapper>(disableConsoleLogging: true);
+
+        #if DEBUG
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            StartDebugTerminal(_logger);
+        }
+        #endif
+
+        _coreApp = new HoscyCoreApp(_logger);
+        var coreAppParams = new HoscyCoreAppStartParameters()
+        {
+            OnProgress = new((s) => Console.WriteLine($"Loading: {s.Replace(Environment.NewLine, " ")}")),
+            ShouldOpenConsoleIfRequested = false,
+            DisableConsoleLog = true,
+            AdditionalContainerInserts = (x) => x.AddSingleton<TestCommandModule>()
+        };
+        _coreApp.Start(coreAppParams);
+        _logger = _coreApp.GetContainer().GetRequiredService<ILogger>().ForContext<CliCoreWrapper>();
+    }
+
+    public void RunLoop()
+    {
+        if (_coreApp is null) throw new ArgumentNullException(nameof(_coreApp));
+        _logger?.Information("Running CLI loop...");
+        var commandModule = _coreApp.GetContainer().GetRequiredService<TestCommandModule>();
+        var verb = " ";
+        var consoleColor = ConsoleColor.White;
+
+        while(true)
+        {
+            Console.Write($"\nEnter a command to execute ('help' for help, 'exit' to exit)\n {verb} > ");
+            var input = Console.ReadLine();
+            Console.WriteLine();
+            if (string.IsNullOrWhiteSpace(input)) continue;
+            if (input.Equals("exit", StringComparison.OrdinalIgnoreCase)) break;
+            _logger?.Information($"Running command: {input}");
+
+            CommandResult result;
+            try
+            {
+                result = commandModule.Execute(input);
+            }
+            catch (Exception e)
+            {
+                _logger?.Error(e, "Failed to execute command");
+                Console.WriteLine($"{e.GetType().FullName}: {e.Message}");
+                result = CommandResult.Error;
+            }
+
+            verb = result switch
+            {
+                CommandResult.Success => " ",
+                CommandResult.Error => "!",
+                CommandResult.NotFound => "?",
+                CommandResult.MissingParameter => ".",
+                _ => "_"
+            };
+            Console.ForegroundColor = consoleColor;
+        }
+        _logger?.Information("Stopping CLI loop...");
+    }
+
+    public void Stop()
+    {
+        _logger?.Information("Stopping CLI...");
+        _coreApp?.Stop();
+        _coreApp = null;
+        _debugProcess?.Kill();
+        _debugProcess?.Dispose();
+        _debugProcess = null;
+    }
+
+    private void StartDebugTerminal(ILogger logger)
+    {
+        if (_debugProcess is not null) return;
+        logger.Information("Starting debug terminal...");
+        Console.WriteLine("Type your preferred terminal to follow logs (ex: 'foot')");
+        var terminal = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(terminal)) return;
+
+        var startInfo = new ProcessStartInfo()
+        {
+            FileName = terminal,
+            Arguments = $"-e tail -f {LogUtils.LogFileName}",
+            UseShellExecute = true
+        };
+        var process = Process.Start(startInfo);
+        _debugProcess = process;
+    }
+}
--- HoscyCli/Program.cs
+++ HoscyCli/Program.cs
@@ -1,58 +1,24 @@
-﻿using System.Diagnostics;
-using HoscyCli.Commands.Modules;
-using HoscyCore;
-using HoscyCore.Utility;
-
-namespace HoscyCli;
+﻿namespace HoscyCli;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Process? debugProcess = null;
-        HoscyCoreApp? coreApp = null;
+        var wrapper = new CliCoreWrapper();
+
         try
         {
-            #if DEBUG
-            var log = LogUtils.CreateTemporaryLogger<Program>(disableConsoleLogging: true);
-            log.Information("a");
-            Console.WriteLine("Type 'y' to follow logs");
-            if (Console.ReadLine() == "y")
-            {
-                var process = new Process();
-                process.StartInfo.FileName = "foot";
-                process.StartInfo.Arguments = $"-e tail -f {LogUtils.LogFileName}";
-                process.StartInfo.UseShellExecute = true;
-                process.Start();
-                debugProcess = process;
-            }
-            #endif
-
-            coreApp = new HoscyCoreApp(log);
-            var coreAppParams = new HoscyCoreAppStartParameters()
-            {
-                OnProgress = new((s) => Console.WriteLine($"Loading: {s.Replace(Environment.NewLine, " ")}")),
-                ShouldOpenConsoleIfRequested = false,
-                DisableConsoleLog = true
-            };
-            coreApp.Start(coreAppParams);
-            while (true)
-            {
-                //todo: logic
-                Console.WriteLine("Type 'exit' to exit");
-                if (Console.ReadLine() == "exit") break;
-            }
+            wrapper.Start();
+            wrapper.RunLoop();
         } 
         catch (Exception e)
         {
             Console.WriteLine($"{e.GetType().FullName}: {e.Message}");
         }
-
+        
         try
         {
-            coreApp?.Stop();
-            debugProcess?.Kill();
-            debugProcess?.Dispose();
+            wrapper.Stop();
         } 
         catch (Exception e)
         {
