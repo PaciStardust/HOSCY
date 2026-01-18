5535335cada2ac2980cc8e6ab5263d384be5b019
PaciStardust <git@paci.dev>
2026-01-18 15:17:42 +0100

    feat(Core): :sparkles: CounterOscMessageHandler

---
+++ HoscyCore/Services/Osc/MessageHandling/Handlers/CounterOscMessageHandler.cs
@@ -0,0 +1,55 @@
+using HoscyCore.Configuration.Modern;
+using HoscyCore.Services.DependencyCore;
+using HoscyCore.Services.Output.Core;
+using LucHeart.CoreOSC;
+using Serilog;
+
+namespace HoscyCore.Services.Osc.MessageHandling.Handlers;
+
+[PrototypeLoadIntoDiContainer(typeof(CounterOscMessageHandler))]
+public class CounterOscMessageHandler(IOutputManagerService output, ConfigModel config, ILogger logger) : IOscMessageHandler
+{
+    private readonly IOutputManagerService _output = output;
+    private readonly ConfigModel _config = config;
+    private readonly ILogger _logger = logger.ForContext<CounterOscMessageHandler>();
+
+    private DateTimeOffset _counterLastDisplay = DateTimeOffset.MinValue;
+
+    public bool HandleMessage(OscMessage message)
+    {
+        var now = DateTimeOffset.UtcNow;
+
+        var counterMatch = _config.Counters_List.FirstOrDefault(x => x.FullParameter().Equals(message.Address, StringComparison.OrdinalIgnoreCase));
+        if (counterMatch is null)
+            return false;
+
+        if (!counterMatch.Enabled || (now - counterMatch.LastUsed).TotalSeconds < counterMatch.Cooldown)
+            return true;
+
+        //todo: config
+        //todo: make most logging use proper quotes
+        counterMatch.Increase();
+        _logger.Debug("Counter \"{counterName}\" ({counterParameter}) increased to {counterCount}", counterMatch.Name, counterMatch.FullParameter(), counterMatch.Count);
+
+        if (!counterMatch.DoDisplay || !_config.Counters_ShowNotification || (now - _counterLastDisplay).TotalSeconds < _config.Counters_DisplayCooldownSeconds)
+            return true;
+
+        var counterString = CreateCounterString(now);
+        if (!string.IsNullOrWhiteSpace(counterString))
+        {
+            _counterLastDisplay = now;
+            _logger.Information("Sending counter notification \"{counterNotification}\"", counterString);
+            _output.SendNotification(counterString, OutputNotificationPriority.Low);
+        }
+        return true;
+    }
+
+    private string CreateCounterString(DateTimeOffset now)
+    {
+        var earliest = now.AddSeconds(-_config.Counters_DisplayDurationSeconds);
+        var matchingCounters = _config.Counters_List
+            .Where(x => x.LastUsed >= earliest)
+            .OrderByDescending(x => x.Count);
+        return string.Join(", ", matchingCounters);
+    }
+}
