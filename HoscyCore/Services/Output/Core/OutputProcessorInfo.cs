2ff160f2aa590113dc7ec560315e447936a27c19
PaciStardust <git@paci.dev>
2026-01-19 15:11:59 +0100

    refactor(OutputRedesign): :truck: Moved OutputProcessorInfoFlags into the Info file as they are always used together

--- HoscyCore/Services/Output/Core/OutputProcessorInfo.cs
+++ HoscyCore/Services/Output/Core/OutputProcessorInfo.cs
@@ -10,3 +10,29 @@ public record OutputProcessorInfo
     public required OutputProcessorInfoFlags Flags;
     public required Type ProcessorType;
 }
+
+/// <summary>
+/// Flags to determine settings to be displayed in UI
+/// </summary>
+[Flags]
+public enum OutputProcessorInfoFlags //todo: these needed?
+{
+    SupportsMessages = 1,
+    SupportsNotifications = 2,
+    SupportsProcessingIndicator = 4,
+    SupportsClearing = 8, //todo: ?
+    OutputAsText = 16,
+    OutputAsAudio = 32, //todo: ?
+    OutputAsOther = 64
+}
+
+/*
+TODO: Sketch
+
+Some module wants to process text
+-> Output Processor
+-> Needs to deliver some info about "What do I allow" (preprocessing, output modes)
+-> Filtering gets applied
+-> Translation gets applied?
+-> Gets sent to processor
+*/
