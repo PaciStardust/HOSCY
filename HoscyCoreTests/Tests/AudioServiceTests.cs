90dd94b0139ac72137a8e4ec75699f47e0cd5bf6
PaciStardust <git@paci.dev>
2026-01-24 13:24:45 +0100

    feat(Core): :sparkles: Utilities for TestBaseForService

--- HoscyCoreTests/Tests/AudioServiceTests.cs
+++ HoscyCoreTests/Tests/AudioServiceTests.cs
@@ -1,5 +1,4 @@
 using HoscyCore.Services.Audio;
-using HoscyCore.Services.DependencyCore;
 using HoscyCoreTests.Utils;
 
 namespace HoscyCoreTests.Tests;
@@ -13,21 +12,18 @@ public class AudioServiceTests : TestBaseForService<AudioServiceTests>
         _audioService = new AudioService(_logger);
         _audioService.Start();
 
-        var status = _audioService.GetCurrentStatus();
-        Assert.That(status, Is.AnyOf(ServiceStatus.Started, ServiceStatus.Processing), "Service status not started");
+        AssertServiceStarted(_audioService);
     }
 
     [Test, Order(int.MaxValue)]
     public void FinalTest()
     {
-        var status = _audioService.GetCurrentStatus();
-        Assert.That(status, Is.AnyOf(ServiceStatus.Started, ServiceStatus.Processing), "Service status not started");
+        AssertServiceStarted(_audioService);
     }
 
     protected override void OneTimeTearDownExtra()
     {
         _audioService.Stop();
-        var status = _audioService.GetCurrentStatus();
-        Assert.That(status, Is.EqualTo(ServiceStatus.Stopped), "Service status not stopped");
+        AssertServiceStopped(_audioService);
     }
 }
--- HoscyCoreTests/Utils/TestBaseForService.cs
+++ HoscyCoreTests/Utils/TestBaseForService.cs
@@ -1,7 +1,21 @@
+using HoscyCore.Services.DependencyCore;
+
 namespace HoscyCoreTests.Utils;
 
 public abstract class TestBaseForService<T> : TestBase<T>
 {
     [Test]
     public void OnlyForStartStop() {}
+
+    protected static void AssertServiceStarted(IStartStopService startStopService)
+    {
+        var status = startStopService.GetCurrentStatus();
+        Assert.That(status, Is.AnyOf(ServiceStatus.Started, ServiceStatus.Processing), "Service status not started");
+    }
+
+    protected static void AssertServiceStopped(IStartStopService startStopService)
+    {
+        var status = startStopService.GetCurrentStatus();
+        Assert.That(status, Is.AnyOf(ServiceStatus.Stopped), "Service status not stopped");
+    }
 }
