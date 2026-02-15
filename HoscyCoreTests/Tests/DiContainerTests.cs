c77d1f997355da341a4cc966d48f60ff34b03ce4
PaciStardust <git@paci.dev>
2026-02-15 20:40:42 +0100

    test(Core): :white_check_mark: more DiContainerTests

--- HoscyCoreTests/Tests/DiContainerTests.cs
+++ HoscyCoreTests/Tests/DiContainerTests.cs
@@ -4,9 +4,47 @@ using HoscyCoreTests.Utils;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
-namespace HoscyCoreTests.Tests;
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace HoscyCoreTests.Tests.DiContainerTests;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
 
-public class DiContainerTests : TestBaseForService<DiContainerTests>
+public class DiContainerStartupTests : TestBase<DiContainerStartupTests>
+{
+    private ConfigModel _config = null!;
+
+    protected override void SetupExtra()
+    {
+        _config = new();
+    }
+
+    [Test]
+    public void StartStopTest()
+    {
+        List<string> progress = [];
+
+        var container = DiContainer.CreateWithAssembly(_logger, _config, null);
+        
+        container.StartServices(x => progress.Add(x));
+        Assert.That(progress, Is.Not.Empty);
+
+        container.StopServices();
+    }
+
+    [Test]
+    public void EmptyTest()
+    {
+        List<string> progress = [];
+
+        var container = DiContainer.Empty();
+        
+        container.StartServices(x => progress.Add(x));
+        Assert.That(progress, Is.Not.Empty);
+
+        container.StopServices();
+    }
+}
+
+public class DiContainerFunctionTests : TestBase<DiContainerFunctionTests>
 {
     private DiContainer _container = null!;
     private readonly ConfigModel _config = new()
