4107529429bd84cbc8695078049b26fb636f4849
PaciStardust <git@paci.dev>
2026-02-27 22:19:45 +0100

    fix(CLI): :bug: fixed preprocessors not reloading on change in cli

--- HoscyCli/Commands/Modules/PreprocessingCommandModule.cs
+++ HoscyCli/Commands/Modules/PreprocessingCommandModule.cs
@@ -1,13 +1,21 @@
 using HoscyCli.Commands.Core;
 using HoscyCore.Configuration.Modern;
 using HoscyCore.Services.Dependency;
+using HoscyCore.Services.Output.Preprocessing;
 
 namespace HoscyCli.Commands.Modules;
 
 [PrototypeLoadIntoDiContainer(typeof(PreprocessingCommandModule))]
-public class PreprocessingCommandModule(ReflectPropEditCommandModule reflectCm) : AttributeCommandModule, ICoreCommandModule
+public class PreprocessingCommandModule
+(   
+    ReflectPropEditCommandModule reflectCm,
+    PartialReplacementOutputPreprocessor preprocessPartial,
+    FullReplacementOutputPreprocessor preprocessFull
+) : AttributeCommandModule, ICoreCommandModule
 {
     private readonly ReflectPropEditCommandModule _reflectCm = reflectCm;
+    private readonly PartialReplacementOutputPreprocessor _preprocessPartial = preprocessPartial;
+    private readonly FullReplacementOutputPreprocessor _preprocessFull = preprocessFull;
 
     public string ModuleName => "Preprocessing";
     public string ModuleDescription => "Configure preprocessing";
@@ -28,13 +36,17 @@ public class PreprocessingCommandModule(ReflectPropEditCommandModule reflectCm) 
     [SubCommandModule(["edit-replace-partial"], "Edit partial replacements")]
     public CommandResult CmdEditReplacePartial()
     {
-        return _reflectCm.SetProperty(nameof(ConfigModel.Preprocessing_ReplacementsPartial));
+        var res = _reflectCm.SetProperty(nameof(ConfigModel.Preprocessing_ReplacementsPartial));
+        _preprocessPartial.ReloadReplacements();
+        return res;
     }
 
     [SubCommandModule(["edit-replace-full"], "Edit full replacements")]
     public CommandResult CmdEditReplaceFull()
     {
-        return _reflectCm.SetProperty(nameof(ConfigModel.Preprocessing_ReplacementsFull));
+        var res = _reflectCm.SetProperty(nameof(ConfigModel.Preprocessing_ReplacementsFull));
+        _preprocessFull.ReloadReplacements();
+        return res;
     }
 
     [SubCommandModule(["ignorechars-replace-full"], "Edit ignored characters for full replacements")]
