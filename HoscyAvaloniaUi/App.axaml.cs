b383e7a16ac68497bfbe0c5bdfbb9ef354a891b9
PaciStardust <git@paci.dev>
2026-04-14 08:16:34 +0200

    fix(Avalonia): :ambulance: avalonia compile fix

--- HoscyAvaloniaUi/App.axaml.cs
+++ HoscyAvaloniaUi/App.axaml.cs
@@ -50,25 +50,25 @@ public partial class App : Application
         }
         // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
         // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-        DisableAvaloniaDataAnnotationValidation();
+        // DisableAvaloniaDataAnnotationValidation();
 
         StartApplication(desktop);
 
         base.OnFrameworkInitializationCompleted();
     }
 
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+    // private void DisableAvaloniaDataAnnotationValidation()
+    // {
+    //     // Get an array of plugins to remove
+    //     var dataValidationPluginsToRemove =
+    //         BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
 
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
-    }
+    //     // remove each entry found
+    //     foreach (var plugin in dataValidationPluginsToRemove)
+    //     {
+    //         BindingPlugins.DataValidators.Remove(plugin);
+    //     }
+    // }
 
     private void StartApplication(IClassicDesktopStyleApplicationLifetime desktop)
     {
