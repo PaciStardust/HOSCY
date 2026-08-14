507d5606ee68bdb28f599e8cc6d655bbd605168d
PaciStardust <git@paci.dev>
2026-08-14 17:20:45 +0200

    feat(Avalonia): :sparkles: saving after counter editor close

--- HoscyAvaloniaUi/ViewModels/SubMenus/ExtrasSubMenuViewModel.cs
+++ HoscyAvaloniaUi/ViewModels/SubMenus/ExtrasSubMenuViewModel.cs
@@ -4,6 +4,8 @@ using HoscyAvaloniaUi.ViewModels.Core;
 using HoscyCore.Configuration.Modern;
 using HoscyCore.Services.Afk;
 using HoscyCore.Services.Dependency;
+using HoscyCore.Utility;
+using Serilog;
 
 namespace HoscyAvaloniaUi.ViewModels.SubMenus;
 
@@ -21,12 +23,14 @@ public class ExtrasSubMenuViewModelImpl : ExtrasSubMenuViewModelBase
 {
     private readonly IAfkService _afk;
     private readonly PopupWindowFactory _popup;
+    private readonly ILogger _logger;
 
-    public ExtrasSubMenuViewModelImpl(ConfigModel config, IAfkService afk, PopupWindowFactory popup)
+    public ExtrasSubMenuViewModelImpl(ConfigModel config, IAfkService afk, PopupWindowFactory popup, ILogger logger)
     {
         Config = config;
         _afk = afk;
         _popup = popup;
+        _logger = logger.ForContext<ExtrasSubMenuViewModelImpl>();
     }
 
     public override void AfkSkipClicked()
@@ -37,6 +41,7 @@ public class ExtrasSubMenuViewModelImpl : ExtrasSubMenuViewModelBase
     public override void EditCountersClicked()
     {
         _popup.OpenEditCounters(Config.Counters_List, null);
+        Config.TrySave(PathUtils.PathConfigFolder, ConfigModelLoader.DEFAULT_FILE_NAME, _logger);
     }
 }
 
