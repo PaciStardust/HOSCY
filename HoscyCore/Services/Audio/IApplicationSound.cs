c953ad5ef0c73bfdbec813bcac9320685ddfc54e
PaciStardust <git@paci.dev>
2026-09-17 14:58:39 +0200

    fix(Avalonia): :lipstick: fixed whisper model selector width

--- HoscyCore/Services/Audio/IApplicationSound.cs
+++ HoscyCore/Services/Audio/IApplicationSound.cs
@@ -2,7 +2,7 @@ using HoscyCore.Services.Core;
 
 namespace HoscyCore.Services.Audio;
 
-public interface IApplicationSound : IService //todo: fix missing sound and misalignment
+public interface IApplicationSound : IService //todo: fix missing sound
 {
     public void PlayMuteSound();
     public void PlayNotificationSound();
